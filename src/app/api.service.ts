8ec3afa598098f7aac87cb7b77dea9d0cee8a49c
alisonavi <bsaulebay@gmail.com>
2025-05-20 11:41:10 +0500

    fetched api and get the data results and page also got the pagination with it

---
+++ src/app/api.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+export interface User {
+  id: number,
+  email: string,
+  name: {
+    first: string;
+    last: string
+  }
+  picture: {
+    thumbnail: string
+  }
+  gender: string,
+}
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+  private url = 'https://randomuser.me/api/?results=50'
+  constructor(private http: HttpClient) {
+   }
+    getUsers(): Observable<User[]> {
+    return this.http
+      .get<{ results: User[] }>(this.url)
+      .pipe(
+        // import { map } from 'rxjs/operators'
+        map((res: { results: User[] }) => res.results)
+      );
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { ApiService, User } from './api.service';
+import { Observable } from 'rxjs';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, MatPaginator, MatPaginatorModule,MatTableModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'filtration';
+export class AppComponent implements OnInit, AfterViewInit {
+  displayedColumns = ['gender', 'first', 'last'];
+  dataSource = new MatTableDataSource<User>([]);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private api: ApiService) {
+  }
+  ngOnInit(): void {
+    this.api.getUsers().subscribe({
+      next: users => this.dataSource.data = users,
+      error: err => console.error(err)
+    })
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
