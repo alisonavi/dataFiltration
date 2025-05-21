8bf053840d86d0874d488c386d025f2dab2f561e
alisonavi <bsaulebay@gmail.com>
2025-05-21 11:31:49 +0500

    added the create person button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,68 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { CommonModule }                                    from '@angular/common';
-import { MatTableModule, MatTableDataSource }              from '@angular/material/table';
-import { MatPaginatorModule, MatPaginator }                from '@angular/material/paginator';
-import { MatSortModule, MatSort }                          from '@angular/material/sort';
-import { MatFormFieldModule }                              from '@angular/material/form-field';
-import { MatSelectModule }                                 from '@angular/material/select';
-import { FormsModule }                                     from '@angular/forms';
-import { ApiService, User }                                from './api.service';
-import { MatButtonModule, MatIconButton } from '@angular/material/button';
-
+import {Component, OnInit,  AfterViewInit, ViewChild, TemplateRef, ChangeDetectionStrategy} from '@angular/core';
+import { CommonModule } from'@angular/common';
+import {MatTableModule, MatTableDataSource} from '@angular/material/table';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import {  MatSortModule, MatSort } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule }from '@angular/material/select';
+import { MatInputModule  } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialog  } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { ApiService, User } from './api.service';
+import {MatIcon} from '@angular/material/icon';
 @Component({
-  standalone:  true,
-  selector:    'app-root',
-  imports:     [
+  standalone: true,
+  selector: 'app-root',
+  changeDetection:ChangeDetectionStrategy.OnPush,
+  imports: [
     CommonModule,
+    FormsModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
     MatFormFieldModule,
-    MatSelectModule,
-    FormsModule,
-    MatButtonModule,
-    MatIconButton
+    MatSelectModule, MatInputModule, MatButtonModule, MatDialogModule, MatIcon, MatDialogModule, MatInputModule
   ],
-  templateUrl: './app.component.html',
-  styleUrls:   ['./app.component.css']
+  templateUrl:'./app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  displayedColumns = ['thumbnail','first','last', 'gender', 'email','city', 'delete'];
+  displayedColumns = [
+    'thumbnail','first','last',
+    'gender','email','city','delete'
+  ];
   dataSource = new MatTableDataSource<User>([]);
   selectedGender = '';
+
+  newUser = {
+    gender: '',
+    email:  '',
+    name:   { first: '', last: '' },
+    picture: { thumbnail: '' },
+    location: { city: '' },
+    id:      0
+  } as User;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort)      sort!: MatSort;
 
-  constructor(private api: ApiService) {}
+  @ViewChild('addPersonDialog') addPersonDialog!: TemplateRef<any>;
+
+  constructor(
+    private api:    ApiService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
-    this.dataSource.filterPredicate = (user, filter) =>
-      !filter || user.gender.toLowerCase() === filter.toLowerCase();
-    this.dataSource.sortingDataAccessor = (user, prop) => {
-      if (prop === 'first') 
-        return user.name.first;
-      if (prop === 'last')  
-        return user.name.last;
-      return (user as any)
-      [prop];
+    this.dataSource.filterPredicate = (u, f) =>
+      !f || u.gender.toLowerCase() === f.toLowerCase();
+    this.dataSource.sortingDataAccessor = (u, prop) => {
+      if (prop === 'first') return u.name.first;
+      if (prop === 'last')  return u.name.last;
+      return (u as any)[prop];
     };
+
     this.api.getUsers().subscribe(users => {
       this.dataSource.data = users;
     });
@@ -54,28 +70,53 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.dataSource.sort      = this.sort;
   }
+
   applyFilters() {
     this.dataSource.filter = this.selectedGender.trim();
     this.paginator.firstPage();
   }
+
   clearFilters() {
     this.selectedGender = '';
     this.dataSource.filter = '';
-    this.sort.active = '';
+    this.sort.active    = '';
     this.sort.direction = '';
   }
-  toggleSort() {
-    const nextDir = this.sort.direction === 'asc' ? 'desc' : 'asc';
-    this.sort.sort({ id: 'first', start: nextDir, disableClear: true });
-  }
-removeUser(user: User) {
-  const idx = this.dataSource.data.indexOf(user);
-  if (idx > -1) {
-    this.dataSource.data.splice(idx, 1);
-    this.dataSource.data = [...this.dataSource.data];
-  }
-}
 
+  toggleSort() {
+    const next = this.sort.direction === 'asc' ? 'desc' : 'asc';
+    this.sort.sort({ id: 'first', start: next, disableClear: true });
+  }
+
+  removeUser(u: User) {
+    const idx = this.dataSource.data.indexOf(u);
+    if (idx > -1) {
+      this.dataSource.data.splice(idx, 1);
+      this.dataSource.data = [...this.dataSource.data];
+    }
+  }
+
+  openAddDialog() {
+    this.newUser = {
+      gender: '',
+      email:  '',
+      name:   { first: '', last: '' },
+      picture: { thumbnail: '' },
+      location: { city: '' },
+      id:      Date.now()
+    };
+
+    const ref = this.dialog.open(this.addPersonDialog, {
+      width: '400px'
+    });
+
+    ref.afterClosed().subscribe((result: User|undefined) => {
+      if (result) {
+        this.dataSource.data = [ result, ...this.dataSource.data ];
+        this.paginator.firstPage();
+      }
+    });
+  }
 }
